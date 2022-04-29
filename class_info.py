e30b8fda552b5ac16a752d92435954fc184c1929
Vodahlyest <104577497+Vodahlyest@users.noreply.github.com>
2022-04-29 07:24:06 +0000

    Update class_info.py

--- class_info.py
+++ class_info.py
@@ -1,16 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-links = ['https://sch2001.ru/index.php?sid=1080', 'https://sch2001.ru/index.php?sid=1378']
-clases = []
-for link in links:
-    source = requests.get(link)
-    soup = BeautifulSoup(source.text, 'html.parser')
-    main_table = soup.find_all('td', width="150px")
-    for tag in main_table:
-        class_name = tag.get_text()
-        print(class_name)
-        clases.append(class_name)
+"""  Парсер классов """
 
-with open("классы", 'w') as f:
+import requests       # импорт нужных бибилиотек
+from bs4 import BeautifulSoup
+
+links = ['link1', 'link2']  # массив с ссылками, откуда нужно парсить классы школы
+clases = []      """ Здесь будут сохранены наши классы """
+for link in links:   """ перебор ссылок из массива """
+    source = requests.get(link)      
+    soup = BeautifulSoup(source.text, 'html.parser')    """Создается объект класса BeautifulSoup для парсинга нужных разделов сайта"""
+    main_table = soup.find_all('td', width="150px")     """Ищем все теги на нужном разделе сайта, где хранятся названия классов"""
+    for tag in main_table:             """Перебираем теги"""
+        class_name = tag.get_text()    """Получаем название класса"""
+        print(class_name)
+        clases.append(class_name)      """И заносим его в массив"""
+
+with open("классы", 'w') as f:         """открываем файл для хранения классов школы и при помощи цикла записываем в него классы"""
     for class_name in clases:
         f.writelines(class_name + ":")
