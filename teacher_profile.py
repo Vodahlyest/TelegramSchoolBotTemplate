6a6a68d756a91d89904f5b40cf548187d6feecda
Vodahlyest <104577497+Vodahlyest@users.noreply.github.com>
2022-04-29 07:31:20 +0000

    Update teacher_profile.py

--- teacher_profile.py
+++ teacher_profile.py
@@ -1,13 +1,14 @@
+""" Парсер профилей учителей """
+
+"""Импрот неоходимых Бибилиотек"""
 import requests
 from bs4 import BeautifulSoup
 
-link = 'https://sch2001.ru/index.php?sid=1045'
-source_list = requests.get(link)
-soup = BeautifulSoup(source_list.text, 'html.parser')
-a_all = soup.find_all('a', style="font-size: 18px; font-family: Monotype Corsiva; text-decoration: none;")
-print(a_all)
-with open("категории учителей", 'w') as f:
-    all_categories = ''
-    for a in a_all:
-        text = a.get_text()
+link = 'link'   """Указываем нужную ссылку с разделом сайта, откуда надо брать профили учителей"""
+source_list = requests.get(link)                 
+soup = BeautifulSoup(source_list.text, 'html.parser')      """Создается объект класса BeautifulSoup для парса необходимых нам даных"""
+category_tags = soup.find_all('a', style="font-size: 18px; font-family: Monotype Corsiva; text-decoration: none;")    """Ищем все теги, в которых содержатся профили учителей"""
+with open("name", 'w') as f:       """Открываем файл для хранения профилей преподователей для записи"""      
+    for category in category_tags: """Перебор всех найденных Тегов и запись содержащейся в ней необходимой нам информации в файл для хранения
+        text = category.get_text()
         f.writelines(text)
